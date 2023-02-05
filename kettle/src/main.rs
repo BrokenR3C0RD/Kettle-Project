8bddd6d78f7e292469a7f88f567a96c22fd51eef
MasterR3C0RD <masterr3c0rd@epochal.quest>
2023-02-05 17:55:13 -0400

    hal: add intrinsics, make kettle buildable

--- kettle/src/main.rs
+++ kettle/src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use defmt_rtt as _;
+use panic_probe as _;
+use lpc11uxx_hal as hal;
+
+#[cortex_m_rt::entry]
+fn entry() -> ! {
+    defmt::println!("Hello, world!");
+
+    let result = hal::rom_data::sidiv(10, 5);
+    defmt::println!("Division result 1 (should miss cache): {}", result);
+
+    let result = hal::rom_data::sidiv(6, 2);
+    defmt::println!("Division result 2 (should hit cache): {}", result);
+
+    for i in 1..5 {
+        defmt::println!("Division results from intrinsic: {}", 10 / i);
+    }
+
+    let usbd = unsafe { &*(hal::rom_data::RomDriverTable::usb()) };
+    let usbd_version = usbd.version;
+
+
+
+    defmt::println!("USB driver version: {:#X}", usbd_version);
+
+    let mut results = [0u32; 4];
+
+    unsafe { hal::rom_data::iap_entry(&[54, 0, 0, 0, 0], &mut results) };
+
+    defmt::println!("PartID = {:#X}", results[1]);
+
+    loop {}
 }
